f754e2d1a2f45354f996dd13939b7b44715380b1
robot <tunwer@naver.com>
2022-05-27 09:28:11 +0900

    test

--- app/src/main/java/com/example/mytour/info_activity.java
+++ app/src/main/java/com/example/mytour/info_activity.java
@@ -3,6 +3,10 @@ package com.example.mytour;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class info_activity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class info_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        ImageView img_info = findViewById(R.id.img_info);
+        TextView txt_info = findViewById(R.id.txt_info);
+        
+        img_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(info_activity.this, "image Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
